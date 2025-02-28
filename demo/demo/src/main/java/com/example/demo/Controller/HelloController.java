e34ceaa505e9ca85721f414e222e5c0084a67de8
montydavid <jdmontaguthv@ufpso.edu.co>
2025-02-27 21:01:49 -0500

    agregar rama 192138

--- demo/demo/src/main/java/com/example/demo/Controller/HelloController.java
+++ demo/demo/src/main/java/com/example/demo/Controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "¡Hola, Mundo cruel!";
+        return "¡Hola, Mundo cruelito!";
     }
 }
